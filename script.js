1ef78912a20edbd2612bcc45d90492781b08cb8a
JordanAdams_MacBook <jsadams27@gmail.com>
2024-08-01 18:16:44 -0400

    adds function to pull today's temp data from Visual Crossing

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('fart')
+const tempDisplayToday = document.getElementById('todays-temp-number')
+
+
+
+async function getWeather() {
+        const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Fort%20Wayne%2C%20IN?unitGroup=metric&key=UB7HSQJ56P9RUPG56M8PFDFB3&contentType=json')
+        const weatherData = await response.json()
+        
+        // console.log(weatherData.currentConditions.temp)
+        
+        tempDisplayToday.textContent = weatherData.currentConditions.temp
+
+}
+
+getWeather();
+
